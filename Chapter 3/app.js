8e89ceb89d339d66fe0f847cad06352eb73cd5cb
Alisha432 <alishu432@gmail.com>
2026-02-28 13:57:41 +0500

    Chapter 3

---
+++ Chapter 3/app.js
@@ -0,0 +1,22 @@
+// Question : 01
+// let age = 20
+// alert ("I m " + age + " years old")
+
+// Question :02
+
+// let visit =  10
+// alert ("You have visited this site " + visit + " times ")
+
+
+// Question : 03
+
+// let birthyear = 2005
+// document.write ("My birthyear is " + birthyear +  " <br> Date type of my ddeclared variable is number")
+
+// Question :04
+
+// let visterName = "jhone deo"
+// let productTittle = "t - shirt"
+// let quanity = "5"
+
+// document.write (visterName + " Ordered " productTittle  + quanity  + " on xyz clothing store ")
