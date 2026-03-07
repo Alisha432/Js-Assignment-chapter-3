ed407e01ed9fe0cf2725d035c2c62f057090bd6d
Alisha432 <alishu432@gmail.com>
2026-03-07 22:15:28 +0500

    Chapter 5 complete

---
+++ Chapter 5/app.js
@@ -0,0 +1,140 @@
+// Question 01
+alert("thanjks")
+
+// let num1 = +prompt( "Enter your First Name")
+// let num2 = +prompt("Enter your Second Name")
+
+// let sum = num1 + num2
+
+// document.write(`Sum of ${num1} and ${num2} is ${sum} `)
+
+
+// Question 02
+// (A section)
+// let num3 = prompt("Enter your First Name")
+// let num5 = prompt("Enter your Second Name")
+// let sub = num3 - num5
+// document.write(`subtraction of ${num3} and ${num5} is ${sub}`)
+
+
+// (B section)
+
+// let num6 = prompt("Enter your First Name")
+// let num7 = prompt("Enter your Second Name")
+// let multiTotal = num6 * num7
+
+// document.write(`Multiplication of ${num6} and ${num7} is ${multiTotal}`)
+
+
+// (C section)
+
+// let num8 = prompt("Enter your First Name")
+// let num9 = prompt("Enter your Second Name")
+
+
+// let Divide = num8 / num9
+
+// document.write(`Division of ${num8} and ${num9} is ${Divide}`)
+
+
+
+// Question 2
+
+// a
+
+// let valueNumbers
+
+// b
+
+// document.write(`Value after variable declaration is`)
+// document.write(`<br />`)
+
+// // c
+// valueNumbers = 5 
+
+// document.write(`Value after variable declaration is ${valueNumbers}`)
+
+// d
+
+// document.write(`intial value is ${num}`)
+// document.write(<br/>)
+
+
+// e
+// num++
+
+// f
+// document.write(`Value after increment is ${num}`)
+// document.write(`<br />`)
+
+// g
+// num = num + 7
+
+// h
+// document.write(`Value after addition is ${num}`)
+// document.write(`<br />`)
+
+// i
+// num--
+
+// j
+// document.write(`Value after decrement is ${num}`)
+// document.write(`<br />`)
+
+// k
+// num = num / 3
+
+// l
+// document.write(`Value after divide by 3 is ${num}`)
+// document.write(`<br />`)
+
+// m
+// document.write(`The output is 0 `)
+
+
+
+// question 4
+// let ticketPrice = 600
+// let qtyTicket = 5
+
+// let multiTicket = ticketPrice * qtyTicket
+
+// document.write(`Ticket cost to buy ${qtyTicket} tickets to a movie is ${multiTicket}PKR`)
+
+
+
+
+// question 5
+// let tableName = 4
+// let count = 1
+
+// document.write(`${tableName} x ${count} = ${tableName}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+// document.write(`<br />`)
+// count++
+// document.write(`${tableName} x ${count} = ${tableName * count}`)
+
+
