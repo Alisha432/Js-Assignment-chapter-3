f4d9a89764c10e2f071bc31c94c598016de62113
Alisha432 <alishu432@gmail.com>
2026-03-07 13:55:50 +0500

    Chapter5

---
+++ Chapter5/app.js
@@ -0,0 +1,22 @@
+// Question 01
+
+// let num1 = +prompt( "Enter your First Name")
+// let num2 = +prompt("Enter your Second Name")
+
+// let sum = num1 + num2
+
+// document.write(`Sum of ${num1} and ${num2} is ${sum} `)
+
+
+// Question 02
+// (A section)
+// let num3 = prompt("Enter your First Name")
+// let num5 = prompt("Enter your Second Name")
+// let sub = num3 - num5
+// document.write(`subtraction of ${num3} and ${num5} is ${sub}`)
+
+
+// (B section)
+
+let num3 = prompt("Enter your First Name")
+let num5 = prompt("Enter your Second Name")
