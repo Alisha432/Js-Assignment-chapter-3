a167d5e831bdf967427551a95d259c05efbcefbf
Alisha432 <alishu432@gmail.com>
2026-03-22 14:41:10 +0500

    chapter 6

---
+++ chapter 6/app.js
@@ -0,0 +1,58 @@
+// Question 01
+
+// let a = 10;
+
+// document.write("Result:<br>");
+// document.write("The value of a is: " + a + "<br>");
+// document.write("--------------------------------<br><br>");
+
+// // ++a
+// document.write("The value of ++a is: " + (++a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// // a++
+// document.write("The value of a++ is: " + (a++) + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// // --a
+// document.write("The value of --a is: " + (--a) + "<br>");
+// document.write("Now the value of a is: " + a + "<br><br>");
+
+// // a--
+// document.write("The value of a-- is: " + (a--) + "<br>");
+// document.write("Now the value of a is: " + a + "<br>");
+
+
+// Question 04
+
+// let name = prompt("Enter your name:");
+// alert("Hello, " + name + "! Welcome!");
+
+// Question 05
+
+
+// let num = prompt("Enter a number:");
+
+// let num = prompt("Enter a number:");
+
+// if (num === "" || num === null) {
+//     num = 5;
+// } else {
+//     num = Number(num);
+// }
+
+// document.write(num + " x 1 = " + (num * 1) + "<br>");
+// document.write(num + " x 2 = " + (num * 2) + "<br>");
+// document.write(num + " x 3 = " + (num * 3) + "<br>");
+// document.write(num + " x 4 = " + (num * 4) + "<br>");
+// document.write(num + " x 5 = " + (num * 5) + "<br>");
+// document.write(num + " x 6 = " + (num * 6) + "<br>");
+// document.write(num + " x 7 = " + (num * 7) + "<br>");
+// document.write(num + " x 8 = " + (num * 8) + "<br>");
+// document.write(num + " x 9 = " + (num * 9) + "<br>");
+// document.write(num + " x 10 = " + (num * 10) + "<br>");
+
+
+
+
+
