7f1ba7dc9d296e01f1ee00e96a0fbadae9e20986
Alisha432 <alishu432@gmail.com>
2026-03-26 14:32:32 +0500

    chapter 9to11

--- chapter 6/app.js
+++ chapter 6/app.js
@@ -23,12 +23,46 @@
 // document.write("Now the value of a is: " + a + "<br>");
 
 
-// Question 04
+
+// question 2
+
+// let a = 2
+// let b = 1
+
+// --a
+// document.write(`The value of a is ${a}`)
+// document.write(`<br />`)
+
+// --a- --b
+
+// 0 - 0
+//   0
+
+// document.write(`The value of a is ${a} and ${b}`)
+// document.write(`<br />`)
+
+
+//   --a - --b + ++b; 
+//   -1 - -1 + 1 
+//   -1 - 0  
+//   a = -1 b = 0
+
+// document.write(`The value of a is ${a} and ${b}`)
+// document.write(`<br />`)
+
+
+//    --a - --b + ++b + b--; 
+
+// document.write(`The value of a is ${a} and ${b}`)
+// document.write(`<br />`)
+
+
+// Question 03
 
 // let name = prompt("Enter your name:");
 // alert("Hello, " + name + "! Welcome!");
 
-// Question 05
+// Question 04
 
 
 // let num = prompt("Enter a number:");
@@ -51,6 +85,35 @@
 // document.write(num + " x 8 = " + (num * 8) + "<br>");
 // document.write(num + " x 9 = " + (num * 9) + "<br>");
 // document.write(num + " x 10 = " + (num * 10) + "<br>");
+
+
+// Question 05
+
+
+let subject1 = +prompt("Enter your first subject name")
+let subject2 = +prompt("Enter your second subject name")
+let subject3 = +prompt("Enter your third subject name")
+
+document.write(`${subject1}`)
+document.write(`${subject2}`)
+document.write(`${subject3}`)
+
+ let sub1Marks = 100
+ let sub2Marks = 100
+ let sub3Marks = 100
+
+ 
+let obt1 = +prompt("Enter your first subject name")
+let  obt2 = +prompt("Enter your second subject name")
+let  obt3 = +prompt("Enter your second subject name")
+
+let obt1Marks = obt1
+let obt2Marks = obt2
+let obt3Marks = obt3
+
+let obtainMarks = obt1Marks + obt2Marks + obt3Marks
+
+document.write(`total number of all subject is ${obt1Marks}  ${obt2Marks} ${obt3Marks}`)
 
 
 
---
+++ chapter9 to 11/app.js
@@ -0,0 +1,88 @@
+// Question 1
+
+// let city = prompt("Enter your city name");
+
+// if (city === "Karachi") {
+//     alert("Welcome to city of lights");
+// }
+
+
+
+// question 
+
+
+// let gender = prompt("Enter your gender:");
+
+// if (gender === "male") {
+//     alert("Good Morning Sir");
+// } else if (gender === "female") {
+//     alert("Good Morning Ma’am");
+// }
+
+// question 3
+
+
+// let color = prompt("Enter traffic signal color:");
+
+// if (color === "red") {
+//     alert("Must Stop");
+// } else if (color === "yellow") {
+//     alert("Ready to move");
+// } else if (color === "green") {
+//     alert("Move now");
+// }
+
+
+// question 4
+
+// let fuel = prompt("Enter remaining fuel in litres:");
+
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// }
+
+
+// question 6
+
+// let marks1 = +prompt("Enter marks for Subject 1:");
+// let marks2 = +prompt("Enter marks for Subject 2:");
+// let marks3 = +prompt("Enter marks for Subject 3:");
+
+// let totalMarks = +prompt("Enter total marks:");
+
+// // Step 2: Calculate total and percentage
+
+// let totalObtained = marks1 + marks2 + marks3;
+// let percentage = (totalObtained / totalMarks) * 100;
+
+// // Step 3: Determine grade and remarks
+
+
+// let grade, remarks;
+
+// if (percentage >= 80) {
+//     grade = "A+";
+//     remarks = "Excellent";
+// } else if (percentage >= 70) {
+//     grade = "A";
+//     remarks = "Very Good";
+// } else if (percentage >= 60) {
+//     grade = "B";
+//     remarks = "Good";
+// } else if (percentage >= 50) {
+//     grade = "C";
+//     remarks = "Needs Improvement";
+// } else {
+//     grade = "Fail";
+//     remarks = "Poor";
+// }
+
+// // Step 4: Display results in 
+
+
+// document.write("<h2>Marks Sheet</h2>");
+// document.write("Total Marks: " + totalMarks + "<br>");
+// document.write("Marks Obtained: " + totalObtained + "<br>");
+// document.write("Percentage: " + percentage.toFixed(2) + "%<br>");
+// document.write("Grade: " + grade + "<br>");
+// document.write("Remarks: " + remarks + "<br>");
