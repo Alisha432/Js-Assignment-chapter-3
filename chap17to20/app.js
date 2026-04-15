66afb78023db779e5c71546ec4d252a8ccfeeef0
Alisha432 <alishu432@gmail.com>
2026-04-15 14:28:04 +0500

    chap 17to20

---
+++ chap17to20/app.js
@@ -0,0 +1,107 @@
+// question 1
+
+// let namne = [[]];
+
+// document.write(name)
+
+
+
+
+
+
+
+
+
+
+// question 2 
+
+
+
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// console.log(arr)
+
+
+
+// question 3
+
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// } 
+
+
+
+
+// question 4
+
+
+// let num = prompt("Enter table number:");
+// let length = prompt("Enter table length:");
+
+// for (let i = 1; i <= length; i++) {
+//   console.log(num + " x " + i + " = " + (num * i));
+// }
+
+
+
+// question 5
+
+
+// let fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   document.write(fruits[i] + "<br>");
+// }
+
+
+
+// question 6
+
+// (a)
+
+// for (let i = 1; i <= 15; i++) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+
+// (b)
+
+// for (let i = 10; i >= 1; i--) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+// (c)
+
+// for (let i = 0; i <= 20; i += 2) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+
+// (d)
+
+
+// for (let i = 1; i <= 19; i += 2) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>");
+
+
+
+// (e)
+
+
+// for (let i = 2; i <= 20; i += 2) {
+//   document.write(i + "k, ");
+// }
+
+
+
+// question 7
