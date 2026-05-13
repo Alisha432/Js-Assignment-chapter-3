acb5448f986022f9071993ee79cc6ee5e19ec3d9
Alisha432 <alishu432@gmail.com>
2026-05-13 21:57:25 +0500

    Arrow function assigment

---
+++ Arrow function Assigment/app.js
@@ -0,0 +1,37 @@
+// question 1
+
+// let add = (a, b) => a + b;
+
+// console.log(add(5, 3));
+
+// question 2
+
+// let check = (num) => 
+//   num % 2 === 0 ? "Even" : "Odd";
+
+// console.log(check(4));
+// console.log(check(7));
+
+// question 3
+
+// let greater = (a, b) => 
+//   a > b ? a : b;
+
+// console.log(greater(10, 5));
+
+
+// question 4
+
+
+// let result = (marks) => 
+//   marks > 40 ? "Pass" : "Fail";
+
+// console.log(result(50));
+// console.log(result(30));
+
+// function check(num) {
+//   return num >= 0 ? "Positive" : "Negative";
+// }
+
+// console.log(check(5));
+// console.log(check(-3));
